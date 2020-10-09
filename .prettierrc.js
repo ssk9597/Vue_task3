8801a07ace84ebad36891866e5ba4d66de51e87d
Ryo Sasaki <wor.ryosasaki@gmail.com>
2020-10-09 10:08:54 +0900

    git init()

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ['plugin:prettier/recommended'],
+    tabWidth: 4, //インデントのスペース数
+    singleQuote: true, //シングルクォートに統一
+    trailingComma: 'es5', //末尾のカンマをどうするか: es5に準拠させる。
+};
