5c59e41a8f3e2dee33124a985982621a4c5af0fd
Ryo Sasaki <wor.ryosasaki@gmail.com>
2020-10-09 10:10:11 +0900

    初期設定 Vuexやコンポーネント未実装

---
+++ vue_task3/src/views/Home.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="home">
+        <h1>ToDoリスト</h1>
+        <form>
+            <input
+                type="radio"
+                name="todo"
+                v-model="value"
+                value="すべて"
+                id="all"
+            />
+            <label for="all">すべて</label>
+            <input
+                type="radio"
+                name="todo"
+                v-model="value"
+                value="作業中"
+                id="work"
+            />
+            <label for="work">作業中</label>
+            <input
+                type="radio"
+                name="todo"
+                v-model="value"
+                value="完了"
+                id="completion"
+            />
+            <label for="completion">完了</label>
+        </form>
+
+        <table>
+            <thead>
+                <th>ID</th>
+                <th>コメント</th>
+                <th>状態</th>
+            </thead>
+            <tbody v-for="item in todoList" :key="item.id">
+                <tr v-if="value === 'すべて'">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.comment }}</td>
+                    <td>
+                        <button @click="changeStatus(item.id)">
+                            {{ item.status }}
+                        </button>
+                    </td>
+                    <td>
+                        <button @click="removeTask(item.id)">削除</button>
+                    </td>
+                </tr>
+
+                <tr v-if="item.status === value">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.comment }}</td>
+                    <td>
+                        <button @click="changeStatus(item.id)">
+                            {{ item.status }}
+                        </button>
+                    </td>
+                    <td>
+                        <button @click="removeTask(item.id)">削除</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <h2>新規タスクの追加</h2>
+        <input type="text" v-model="task" />
+        <button @click="addTask()">追加</button>
+    </div>
+</template>
+
+<script>
+import SwitchTab from '@/components/SwitchTab.vue';
+import DisplayTodo from '@/components/DisplayTodo.vue';
+import AddTodo from '@/components/AddTodo.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        SwitchTab,
+        DisplayTodo,
+        AddTodo,
+    },
+    data() {
+        return {
+            task: '',
+            todoList: [],
+            id: 0,
+            status: '作業中',
+            value: 'すべて',
+        };
+    },
+    methods: {
+        addTask() {
+            for (let i = 0; i <= this.todoList.length; i++) {
+                this.id = i;
+            }
+
+            this.todoList.push({
+                id: this.id,
+                comment: this.task,
+                status: this.status,
+            });
+
+            this.task = '';
+        },
+        removeTask(num) {
+            this.todoList.splice(num, 1);
+
+            this.todoList.forEach((todo, index) => {
+                todo.id = index;
+            });
+        },
+        changeStatus(num) {
+            if (this.todoList[num].status === '作業中') {
+                this.todoList[num].status = '完了';
+            } else {
+                this.todoList[num].status = '作業中';
+            }
+        },
+    },
+};
+</script>
